6cdf5375bf74f6cdd94ffd0dc69a401177fa93c5
Gordon Lou <gordonlou0721@gmail.com>
2025-10-26 16:47:39 +1100

    recursion of quick select and fixed bugs regarding pivot

--- randomMedian.cpp
+++ randomMedian.cpp
@@ -4,24 +4,26 @@
 int findingMedian(std::vector<int>& input, int median) {
 	// selecting last element of the array as the pivot
 	int pivot = input.size() - 1;
-	int greater = input.size() - 1;
-	int lesser = 0;
+	int upperBound = input.size() - 1;
+	int lowerBound = 0;
 	int currentNumber = 0;
-	quickSelect(input, median, lesser, greater);
+	quickSelect(input, median, lowerBound, upperBound);
 
 	return 0;
 }
 
-int quickSelect(std::vector<int>& input, int median, int lesser, int greater) {
-	int pivot = greater;
-	int j = lesser;
-	int i = lesser - 1;
+int quickSelect(std::vector<int>& input, int median, int lowerBound, int upperBound) {
+
+	int pivot = upperBound;
+	std::cout << "\n" << "pivot " << pivot << "\n";
+	int j = lowerBound;
+	int i = lowerBound - 1;
 
 	while (j != pivot) {
 		//std::cout << input.at(j);
 		if (input.at(j) < input.at(pivot)) {
 			++i;
-			std::cout << " i " << input.at(i) << " j " << input.at(j) << "\n";
+	//		std::cout << " i " << input.at(i) << " j " << input.at(j) << "\n";
 			std::swap(input.at(i), input.at(j));
 		}
 		++j;
@@ -29,14 +31,31 @@ int quickSelect(std::vector<int>& input, int median, int lesser, int greater) {
 	++i;
 	std::swap(input.at(i), input.at(pivot));
 
-	if (pivot == median) {
-		return input.at(median);
-	}
-
-	std::cout << "\n";
 	for (int i : input) {
 		std::cout << i << "\n";
 	}
+
+	if (input.at(i) == median) {
+		std::cout << " \nhi ";
+		return input.at(median);
+	}
+	else if (median > input.at(i)) {
+		std::cout << "\nlowerBound " << lowerBound << "\n";
+		lowerBound = i + 1;
+		std::cout << "\n new lowerBound " << lowerBound << "\n";
+		quickSelect(input, median, lowerBound, upperBound);
+	}
+	else if (median < input.at(i)) {
+		std::cout << "upperBound " << upperBound << "\n";
+		upperBound = i - 1;
+		std::cout << "\n new upperBound " << upperBound << "\n";
+		quickSelect(input, median, lowerBound, upperBound);
+	}
+
+	std::cout << "\n";
+	
+
+
 
 
 
