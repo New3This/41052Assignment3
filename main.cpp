dfae7040dbc1c2b4fda1e6b49a2fad9205670280
Gordon Lou <gordonlou0721@gmail.com>
2025-10-26 17:12:07 +1100

    now returns median

--- main.cpp
+++ main.cpp
@@ -1,12 +1,21 @@
 #include "randomMedian.hpp"
 #include "gtest/gtest.h"
 
+TEST(emptySet, ex1) {
+	std::vector<int> input{};
+	int median = ((input.size() + 1) / 2) - 1;
+	int foundMedian = findingMedian(input, median);
+	EXPECT_EQ(foundMedian, median);
+}
+
 TEST(odd5, ex6) {
 	std::vector<int> input{ 4, 0, 3, 2, 1 };
 	int median = ((input.size() + 1) / 2) - 1;
 	int foundMedian = findingMedian(input, median);
 	EXPECT_EQ(foundMedian, median);
 }
+
+
 
 int main(int argc, char* argv[]) {
 	::testing::InitGoogleTest(&argc, argv);
--- randomMedian.cpp
+++ randomMedian.cpp
@@ -3,27 +3,27 @@
 
 int findingMedian(std::vector<int>& input, int median) {
 	// selecting last element of the array as the pivot
+
+
+
 	int pivot = input.size() - 1;
 	int upperBound = input.size() - 1;
 	int lowerBound = 0;
 	int currentNumber = 0;
-	quickSelect(input, median, lowerBound, upperBound);
+	int foundMedian = quickSelect(input, median, lowerBound, upperBound);
 
-	return 0;
+	return foundMedian;
 }
 
 int quickSelect(std::vector<int>& input, int median, int lowerBound, int upperBound) {
 
 	int pivot = upperBound;
-	std::cout << "\n" << "pivot " << pivot << "\n";
 	int j = lowerBound;
 	int i = lowerBound - 1;
 
 	while (j != pivot) {
-		//std::cout << input.at(j);
 		if (input.at(j) < input.at(pivot)) {
 			++i;
-	//		std::cout << " i " << input.at(i) << " j " << input.at(j) << "\n";
 			std::swap(input.at(i), input.at(j));
 		}
 		++j;
@@ -35,24 +35,20 @@ int quickSelect(std::vector<int>& input, int median, int lowerBound, int upperBo
 	for (int i : input) {
 		std::cout << i << "\n";
 	}
-
+	
 	if (median == input.at(i)) {
-		std::cout << " \nhi ";
-		return input.at(i);
+		int answer = input.at(i);
+		return answer;
 	}
 
 	else if (median > input.at(i)) {
-		std::cout << "\nlowerBound " << lowerBound << "\n";
 		lowerBound = i + 1;
-		std::cout << "\n new lowerBound " << lowerBound << "\n";
 		quickSelect(input, median, lowerBound, upperBound);
 	}
 
-	else if (median < input.at(i)) {
-		std::cout << "upperBound " << upperBound << "\n";
+	else if (median < input.at(i)) {	
 		upperBound = i - 1;
-		std::cout << "\n new upperBound " << upperBound << "\n";
 		quickSelect(input, median, lowerBound, upperBound);
 	}
-	return -1;
+
 }
